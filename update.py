0853a00244a68da6b37e04cd319885efbf43e70f
Guangyang Li <gli@crowdskout.com>
2016-12-07 20:44:58 +0800

    Use Alfred Workflow replacing old update method

---
+++ update.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+from workflow import Workflow, ICON_INFO
+
+
+GITHUB_SLUG = 'ligyxy/Crowdskout-Alfred-Workflow'
+VERSION = open(os.path.join(os.path.dirname(__file__),
+                            'version')).read().strip()
+
+log = None
+
+
+def main(wf):
+    query = None
+    if len(wf.args):
+        query = wf.args[0]
+
+    wf.add_item('Current version : {}'.format(VERSION),
+                'Version currently installed', icon=ICON_INFO)
+
+    wf.send_feedback()
+
+
+if __name__ == '__main__':
+    update_settings = {'github_slug': GITHUB_SLUG, 'version': VERSION}
+    wf = Workflow(update_settings=update_settings)
+    log = wf.logger
+
+    if wf.update_available:
+        # Add a notification to top of Script Filter results
+        wf.add_item(u'New version available',
+                    u'Action this item to install the update',
+                    autocomplete=u'workflow:update',
+                    icon=ICON_INFO)
+
+    sys.exit(wf.run(main))
